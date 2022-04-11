1b5bd2eee7c4b5d6ed0fa0635df10ef948596ebd
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-04-11 13:18:27 +0700

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -18,6 +18,8 @@ app.config["DISCORD_BOT_TOKEN"] = os.environ["DISCORD_BOT_TOKEN"]
 app.config["MONGO_URL"] = os.environ["MONGO_URL"]
 app.config["WEBHOOK_HOSTNAME"] = os.environ["WEBHOOK_HOSTNAME"]
 mongo = pymongo.MongoClient(app.config["MONGO_URL"])["Auth"]["Token"]
+from werkzeug.routing import Rule
+app.url_map.add(Rule('/', endpoint='github'))
 
 
 @app.route("/github/<token>")
